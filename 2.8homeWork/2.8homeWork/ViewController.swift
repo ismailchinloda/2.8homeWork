a8e5356851a09aec1084c3e321a914a5fc2a1314
ismailchinloda <113357847+ismailchinloda@users.noreply.github.com>
2023-02-04 20:25:57 +0600

    Add files via upload

---
+++ 2.8homeWork/2.8homeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.8homeWork
+//
+//  Created by Ismail Chinloda on 4/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
